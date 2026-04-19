fc7984b4566a467e63a4cc7a102780462d82febb
Yunshan <yunshan0611@gmail.com>
2026-04-19 20:01:40 +0200

    added model och trader

--- ROUND2/Trader.py
+++ ROUND2/Trader.py
@@ -1,1 +1,12 @@
+from ROUND2.datamodel import OrderDepth, UserId, TradingState, Order
+from typing import List, Dict
+import json
+
+class Trader:
+    def bid(self):
+        bid = 0  # Vi bestämmer sen vad vi ska buda
+        return 
+
+    def run(self):
+        """run implementation"""
 
